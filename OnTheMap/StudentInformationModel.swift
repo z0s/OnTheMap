e82c50459afb9185db6e1d4a51ef82f82de87321
Zeus <masgar001@gmail.com>
2016-07-29 20:18:52 -0700

    feat: student info model

--- OnTheMap/StudentInformationModel.swift
+++ OnTheMap/StudentInformationModel.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+let StudentInfoUpdatedNotification = "StudentInfoUpdatedNotification"
+struct StudentInformationModel {
+    
+    // Student Location Creation Key
+    static let createdAtKey = "createdAt"
+    
+    // First Name Key
+    static let firstNameKey = "firstName"
+    
+    // Last Name Key
+    static let lastNameKey = "lastName"
+    
+    // Latitude Key
+    static let latitudeKey = "latitude"
+    
+    // Longitude Key
+    static let longitudeKey = "longitude"
+    
+    // Map String Data Key
+    static let mapStringKey = "mapString"
+    
+    // Student URL Key
+    static let mediaURLKey = "mediaURL"
+    // Object ID Key
+    static let objectIdKey = "objectId"
+    
+    // Unique Key
+    static let uniqueKeyKey = "uniqueKey"
+    
+    // Updated At Key
+    static let updatedAtKey = "updatedAt"
+    
+    static var studs = [StudentInformation]()
+    
+
+    static func populateStudentList(withStudents newStudents: [[String: AnyObject]]) {
+        studs.removeAll()
+        
+        for student1 in newStudents {
+            let newStudent = StudentInformation(student1)
+            StudentInformationModel.studs.append(newStudent)
+        }
+        
+        let note = NSNotification(name: StudentInfoUpdatedNotification, object: nil)
+        NSNotificationCenter.defaultCenter().postNotification(note)
+    }
+}
