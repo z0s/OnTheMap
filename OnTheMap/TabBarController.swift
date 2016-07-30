d69b466748249749f345fa1462336dcf886df558
Zeus <masgar001@gmail.com>
2016-07-29 20:18:41 -0700

    feat: Tab Bar Controller Updates

--- OnTheMap/TabBarController.swift
+++ OnTheMap/TabBarController.swift
@@ -6,8 +6,68 @@
 //  Copyright © 2016 z0s. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class TabBarController: UITabBarController {
-
+    
+    // MARK: - Constants
+    
+    let logoutButtonTitle = "Logout"
+    let pinImageName = "pin"
+    let unwindFromLogoutButtonSegueID = "unwindFromLogoutButton"
+    let tabBarPinToInfoPostingViewSegueID = "tabBarPinToInfoPostingViewSegue"
+    
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Overrides
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // set up navigation bar items
+        navigationItem.title = "On The Map"
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: logoutButtonTitle, style: UIBarButtonItemStyle.Plain, target: self, action: #selector(doLogout))
+        
+        let pinButton = UIBarButtonItem(image: UIImage(named: pinImageName), style: UIBarButtonItemStyle.Plain, target: self, action: #selector(postInformation))
+        let refreshButton = UIBarButtonItem(barButtonSystemItem: .Refresh, target: self, action: #selector(retrieveUserData))
+        
+        // note: right bar buttons in array appear on nav bar right to left
+        navigationItem.rightBarButtonItems = [refreshButton, pinButton]
+        
+        // do initial data call
+        retrieveUserData()
+        UdacityAPI.getUserInfo()
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // refresh data when the view appears again
+        retrieveUserData()
+    }
+    
+    // MARK: - Selectors
+    
+    /// Segues back (unwinds) to logout function
+    func doLogout() {
+        self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    /// Segues to Information Posting view
+    func postInformation() {
+        performSegueWithIdentifier(tabBarPinToInfoPostingViewSegueID, sender: nil)
+    }
+    
+    
+    /// Calls out for user information
+    func retrieveUserData() {
+        ParseAPI.retrieveMapData()
+    }
+    
 }
+
